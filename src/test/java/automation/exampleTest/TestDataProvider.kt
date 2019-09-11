5753879ab3eb426e813709d432fd516c8eb5216d
AliceCarroll239 <dmutrynoraev@gmail.com>
2019-09-11 17:46:49 +0300

    some reflection

--- src/test/java/automation/exampleTest/TestDataProvider.kt
+++ src/test/java/automation/exampleTest/TestDataProvider.kt
@@ -4,15 +4,20 @@ import org.testng.annotations.Test
 import automation.data.Test1SampleData
 import automation.data.Test2SampleData
 import utils.DataProviderClass
+import kotlin.reflect.full.declaredMemberProperties
 
 class TestDataProvider {
     @Test(priority = 1, dataProvider = "dataProvider", dataProviderClass = DataProviderClass::class)
     fun test1(test1SampleData: Test1SampleData) {
-        println(test1SampleData.stringParam1 + " " + test1SampleData.booleanParam1.toString())
+        test1SampleData.javaClass.kotlin.declaredMemberProperties.forEach {
+            println("Param ${it.name}  of type ${it.returnType} with value ${it.get(test1SampleData)}")
+        }
     }
 
     @Test(priority = 2, dataProvider = "dataProvider", dataProviderClass = DataProviderClass::class)
     fun test2(test2SampleData: Test2SampleData) {
-        println(test2SampleData.stringParam2 + " " + test2SampleData.intParam2.toString())
+        test2SampleData.javaClass.kotlin.declaredMemberProperties.forEach {
+            println("Param ${it.name}  of type ${it.returnType} with value ${it.get(test2SampleData)}")
+        }
     }
 }
